c26fbb5b77f6839bc15413d1857a6ab5d04565c3
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-05-01 21:51:32 +0100

    Driver Utility

---
+++ Selenium-WebdriverJS-Mocha-Template/driverutil/driverutil.js
@@ -0,0 +1,24 @@
+import "chromedriver";
+import "geckodriver";
+import "iedriver";
+import "edgedriver";
+import "operadriver";
+import { Builder } from "selenium-webdriver";
+
+export default function getDriver(browser) {
+    var browsername = browser == undefined ? 'chrome' : browser;
+    switch (browsername.toUpperCase()) {
+        case 'FIREFOX':
+            return new Builder().forBrowser('firefox').build();
+        case 'CHROME':
+            return new Builder().forBrowser('chrome').build();
+        case 'IE':
+            return new Builder().forBrowser('internet explorer').build();
+        case 'EDGE':
+            return new Builder().forBrowser('MicrosoftEdge').build();
+        case 'OPERA':
+            return new Builder().forBrowser('opera').build();
+        default:
+            return new Builder().forBrowser('chrome').build();
+    }
+};
