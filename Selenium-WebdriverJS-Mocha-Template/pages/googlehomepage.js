bfc7a8764ef706f629c087f6f756ab815a6186ff
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-05-01 21:54:15 +0100

    Page Object for Google Search Home Page

---
+++ Selenium-WebdriverJS-Mocha-Template/pages/googlehomepage.js
@@ -0,0 +1,21 @@
+import {By, until, Key} from "selenium-webdriver";
+
+export default function (driver) {
+    const pageobjects = {
+        searchbox: By.id('lst-ib')
+    };
+
+    return {
+        url: "https://www.google.com/",
+        waitUntilVisible: function() {
+            return driver.wait(until.elementLocated(pageobjects.searchbox));
+        },
+        navigate: function() {
+            driver.navigate().to(this.url);
+            return this.waitUntilVisible();
+        },
+        searchfor: function (text) {
+            return driver.findElement(pageobjects.searchbox).sendKeys(text + Key.RETURN);
+        }
+    };
+}
