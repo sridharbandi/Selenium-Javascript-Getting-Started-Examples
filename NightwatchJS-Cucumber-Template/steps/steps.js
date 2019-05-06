25e39e720bc78ac69b14083437f34b61bef8c344
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-05-06 20:21:49 +0100

    Changes to arrow functions

--- NightwatchJS-Cucumber-Template/steps/steps.js
+++ NightwatchJS-Cucumber-Template/steps/steps.js
@@ -3,15 +3,15 @@ import { client } from 'nightwatch-api';
 var GoogleSearchPage = client.page.googlesearchpage();
 var SearchResultsPage = client.page.searchresultspage();
 
-Given(/^User is on Google search page$/, async function () {
+Given(/^User is on Google search page$/, async () => {
     await GoogleSearchPage.navigate();
 });
 
-When(/^User searches for Selenium$/, async function () {
+When(/^User searches for Selenium$/, async () => {
     await GoogleSearchPage.searchFor('Selenium');
 });
 
-Then(/^User can see Selenium results$/, async function () {
+Then(/^User can see Selenium results$/, async () => {
     SearchResultsPage.expect.element('@linkselenium').to.be.visible;
 });
 
