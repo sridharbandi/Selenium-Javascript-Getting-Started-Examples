5d7d9545c4b8ec4f95bbcdbbba640c942d7a3b86
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-05-06 19:40:32 +0100

    Cleaned Pages

--- NightwatchJS-Cucumber-Template/pages/googlesearchpage.js
+++ NightwatchJS-Cucumber-Template/pages/googlesearchpage.js
@@ -1,16 +1,14 @@
-var googleCommands = {
+const googleCommands = {
     searchFor: function(searchtext) {
         return this.waitForElementVisible('@searchbox', 1000)
             .setValue('@searchbox', [searchtext, this.api.Keys.ENTER]);
     }
 };
 
-module.exports = {
-    url: 'http://google.com',
-    commands: [googleCommands],
-    elements: {
-        searchbox: {
-            selector: 'input[type=text]'
-        }
+export const url = 'http://google.com';
+export const commands = [googleCommands];
+export const elements = {
+    searchbox: {
+        selector: 'input[type=text]'
     }
 };
--- NightwatchJS-Cucumber-Template/pages/searchresultspage.js
+++ NightwatchJS-Cucumber-Template/pages/searchresultspage.js
@@ -1,8 +1,6 @@
-module.exports = {
-    elements: {
-        linkselenium: {
-            selector: '//h3[text()="Selenium - Web Browser Automation"]',
-            locateStrategy: 'xpath'
-        }
+export const elements = {
+    linkselenium: {
+        selector: '//h3[text()="Selenium - Web Browser Automation"]',
+        locateStrategy: 'xpath'
     }
 };
