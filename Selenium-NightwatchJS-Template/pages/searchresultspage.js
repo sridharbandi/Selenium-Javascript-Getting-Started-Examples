95466233ef7ff0177100a4370a4f851ea3078fdc
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-05-10 20:46:45 +0100

    Moved to NightwatchJS Template

--- Selenium-NightwatchJS-Template/pages/searchresultspage.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    elements: {
-        linkselenium: {
-            selector: '//h3[text()="Selenium - Web Browser Automation"]',
-            locateStrategy: 'xpath'
-        }
-    }
-};
